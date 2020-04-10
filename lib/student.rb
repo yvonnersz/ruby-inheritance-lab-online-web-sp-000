146ad24e7b0588f6a210831c244dfd3b0e743cbb
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 05:29:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,8 @@
 class Student < User
   attr_accessor :first_name, :last_name
 
-
+  def initialize(first_name,last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @@all = []
 end
